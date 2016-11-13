4d887f6462d833a6e6eafb1e2e7fb1e455888e84
Benjamin Pequet <pequet@Benjamin-Pequets.local>
2016-11-13 17:43:37 -0500

    First commit

--- install_files/includes/inc_sites_empty.sh
+++ install_files/includes/inc_sites_empty.sh
@@ -9,6 +9,8 @@ source $dir_includes/inc_loaded.sh
 # ----------------------------------------------------------------------
 
     ee site create $site --php7
+
+    if [[ $this_privateip == "" ]]; then this_privateip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'); fi
     echo -e '<!DOCTYPE html><html><head><title>Welcome to '$site'!</title></head><body><p>Welcome to <?php print "<b>"; ?>'$site'<?php print "</b>"; ?>! ('$(echo $this_privateip | cut -d . -f 4)')</p></body></html>' > /var/www/$site/htdocs/index.php
     
 # ----------------------------------------------------------------------
