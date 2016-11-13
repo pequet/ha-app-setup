756b5519bd1a49802997c00c9d0ce8f20c47bf93
Benjamin Pequet <pequet@Benjamin-Pequets.local>
2016-11-13 17:20:56 -0500

    First commit

--- install_files/includes/inc_sites_empty.sh
+++ install_files/includes/inc_sites_empty.sh
@@ -9,7 +9,7 @@ source $dir_includes/inc_loaded.sh
 # ----------------------------------------------------------------------
 
     ee site create $site --php7
-    echo -e '<!DOCTYPE html><html><head><title>Welcome to '$site'!</title></head><body><p>Welcome to <?php print "<b>"; ?>'$site'<?php print "</b>"; ?>!</p></body></html>' > /var/www/$site/htdocs/index.php
+    echo -e '<!DOCTYPE html><html><head><title>Welcome to '$site'!</title></head><body><p>Welcome to <?php print "<b>"; ?>'$site'<?php print "</b>"; ?>! ('$(echo $this_privateip | cut -d . -f 4)')</p></body></html>' > /var/www/$site/htdocs/index.php
     
 # ----------------------------------------------------------------------
 
